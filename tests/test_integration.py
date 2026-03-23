192379d7886f617076b2a6eb2189dca34e15de92
WM <niebieskiarbuz@onet.pl>
2026-03-23 09:07:34 +0100

    Dodanie wlasnych testow jednostkowych i integracyjnych

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,7 @@
 from src.models import Apartment
 from src.manager import Manager
 from src.models import Parameters
+import json
 
 
 def test_load_data():
@@ -14,3 +15,15 @@ def test_load_data():
     for apartment_key, apartment in manager.apartments.items():
         assert isinstance(apartment, Apartment)
         assert apartment.key == apartment_key
+
+def test_tenant_names():
+    parameters = Parameters()
+    manager = Manager(parameters)
+
+    names =[
+        "Jan Nowak",
+        "Adam Kowalski",
+        "Ewa Adamska",
+    ]
+    for tenant in manager.tenants.values():
+        assert tenant.name in names
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,7 +1,51 @@
 import pytest
 
 from pydantic import ValidationError
-from src.models import Apartment
+from src.models import Apartment, Tenant
+
+
+
+def test_tenant_fields():
+    data = Tenant(
+        name="tenant-test",
+        apartment="Test Apartment",
+        room="Test Room",
+        rent_pln=100.0,
+        deposit_pln=150.0,
+        date_agreement_from="Test Date From",
+        date_agreement_to="Test Date To"
+    )
+    assert data.name == "tenant-test"
+    assert data.apartment == "Test Apartment"
+    assert data.room == "Test Room"
+    assert data.rent_pln == 100.0
+    assert data.deposit_pln == 150.0
+    assert data.date_agreement_from == "Test Date From"
+    assert data.date_agreement_to == "Test Date To"
+
+def test_tenant_from_dict():
+    data = {
+        "name":"tenant-test",
+        "apartment":"Test Apartment",
+        "room":"Test Room",
+        "rent_pln": 100.0,
+        "deposit_pln":150.0,
+        "date_agreement_from":"Test Date From",
+        "date_agreement_to":"Test Date To"
+    }
+    tenant = Tenant(**data)
+    assert tenant.name == data["name"]
+    assert tenant.apartment == data["apartment"]
+    assert tenant.room == data["room"]
+    assert tenant.rent_pln == data["rent_pln"]
+    assert tenant.deposit_pln == data["deposit_pln"]
+    assert tenant.date_agreement_from == data["date_agreement_from"]
+    assert tenant.date_agreement_to == data["date_agreement_to"]
+
+    data['rent_pln'] = "string"
+    with pytest.raises(ValidationError):
+        Tenant(**data)
+
 
 
 def test_apartment_fields():
@@ -19,7 +63,7 @@ def test_apartment_fields():
     assert data.name == "Test Apartment"
     assert data.location == "Test Location"
     assert data.area_m2 == 50.0
-    assert len(data.rooms) == 2
+    assert len(data.rooms) == 2 # if assert==3 :, f"Oczekiwano 3 mieszkań, ale znaleziono {len(data.rooms)}"
 
 
 def test_apartment_from_dict():
